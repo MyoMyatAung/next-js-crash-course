11409c61e9d459d368cb5d395ba59a7146e191c0
Myo Myat Aung <myatmyo299@gmail.com>
2023-10-15 04:16:40 +0630

    Initial commit

---
+++ helpdesk/app/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "./restaurant-logo.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Image
+        src={Logo}
+        alt="Logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  );
+}
